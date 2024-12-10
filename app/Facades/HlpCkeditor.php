143959f9918d6a2641418ea63c010474bcf7dfc9
Ly Gia Hao <89515363+giahao02@users.noreply.github.com>
2024-12-10 21:01:21 +0700

    Update HlpCkeditor.php

--- app/Facades/HlpCkeditor.php
+++ app/Facades/HlpCkeditor.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 class HlpCkeditor extends Facade
 {
+    // Sử dụng facades thay cho alias đã bị bỏ ở laravel 11
     protected static function getFacadeAccessor()
     {
         return 'ckeditorhelper';
